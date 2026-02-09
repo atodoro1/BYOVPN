d26a3804254a1bf236ab6322ea73efbed3eea2e4
atodoro1 <2angel.todorov@gmail.com>
2026-02-08 19:57:18 -0500

    should work

--- client/pulumi/__main__.py
+++ client/pulumi/__main__.py
@@ -2,7 +2,10 @@
 
 from tokenize import group
 from turtle import pu
+from util import *
 
+import os
+import time
 import pulumi
 import pulumi_aws as aws
 
@@ -29,21 +32,58 @@ group = aws.ec2.SecurityGroup('web-secgrp',
                                     {'protocol': 'udp', 'to_port': WG_SERVER_PORT, 'from_port': 0, 'cidr_blocks': ['0.0.0.0/0']}])
 
 
+server_private_key, server_public_key = generate_key_pair()
+client_private_key, client_public_key = generate_key_pair()
+
+server_config = generate_server_config(WG_SERVER_PORT, server_private_key, client_public_key)
+
 # Adding the user data script to install WireGuard on the EC2 instance
 user_data_script = f"""#!/usr/bin/env bash
-sudo apt-get update
-sudo apt install -y  wireguard wireguard-tools openresolv
-sudo systemctl enable wg-quick@wg0
-sudo systemctl start wg-quick@wg0
-"""
+apt-get update
+apt install -y  wireguard wireguard-tools openresolv
 
+cat << EOF > /etc/wireguard/wg0.conf
+{server_config}
+EOF
+
+IFACE="$(ip route show default | awk '{{print $5; exit}}')"
+sed -i "s/IFACE/${{IFACE}}/g" /etc/wireguard/wg0.conf
+
+chmod 600 /etc/wireguard/wg0.conf
+
+systemctl enable wg-quick@wg0
+systemctl start wg-quick@wg0
+"""
 
 # Create an AWS resource (EC2 Instance)
 instance = aws.ec2.Instance('web-server-vpn',
     instance_type='t3.micro',
     ami=ubuntu.id,
     tags={'Name': 'VPN_Server'},
-    vpc_security_group_ids=[group.id],)
+    vpc_security_group_ids=[group.id],
+    user_data=user_data_script)
+
+server_public_ip = instance.public_ip
+client_public_ip = get_public_ip()
+
+client_config = generate_client_config(WG_SERVER_PORT, client_private_key, 
+                                       server_public_key, server_public_ip, f"{client_public_ip}/32")
+
+with open("/etc/wireguard/client.conf", "w") as f:
+    f.write(client_config)
+
+os.chmod("/etc/wireguard/client.conf", 600)
+
+
+# Wait until the instance is running before trying to bring up the WireGuard interface
+while True:
+    instance_state = aws.ec2.get_instance(instance_id=instance.id).state
+    if instance_state == "running":
+        break
+    time.sleep(5)
+
+os.system("wg-quick up client.conf")
+
 
 # Export the ID of the instance
 pulumi.export('instance_id', instance.id)
---
+++ client/pulumi/util.py
@@ -0,0 +1,44 @@
+from typing import Tuple
+from jinja2 import Template
+
+import requests
+import subprocess
+
+
+def generate_key_pair() -> Tuple(str, str):
+    """Generates a WireGuard key pair using the wg command-line tool in the form (private_key, public_key)."""
+    private_key = subprocess.check_output(['wg', 'genkey']).decode('utf-8').strip()
+    public_key = subprocess.check_output(['wg', 'pubkey'], input=private_key.encode('utf-8')).decode('utf-8').strip()
+    return private_key, public_key
+
+
+def generate_client_config(server_listen_port: int, client_private_key: str, server_public_key: str,
+                        server_public_ip: str, allowed_ips: str) -> str:
+    """Writes the WireGuard client configuration to a file."""
+    template_config = open("wg-conf/client.conf.j2")
+    rendered = Template(template_config.read()).render({"server_listen_port": server_listen_port,
+                                                        "client_private_key": client_private_key,
+                                                        "server_public_key": server_public_key,
+                                                        "server_public_ip": server_public_ip,
+                                                        "allowed_ips": allowed_ips})
+    return rendered
+
+
+def generate_server_config(server_listen_port: int, server_private_key: str, client_public_key: str) -> None:
+    """Writes the WireGuard server configuration to a file."""
+    template_config = open("wg-conf/wg0.conf.j2")
+    rendered = Template(template_config.read()).render({"server_listen_port": server_listen_port,
+                                                        "server_private_key": server_private_key,
+                                                        "client_public_key": client_public_key})
+    return rendered
+
+
+def get_public_ip() -> str:
+    url = "https://ipv4.icanhazip.com/"
+    try:
+        response = requests.get(url)
+        public_ip = response.text.strip()
+        return public_ip
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching IP: {e}")
+        return None
